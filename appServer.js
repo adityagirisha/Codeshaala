fe3cd7a8822c7aceb2d66cb2abf8f78d2d9eeb0d
baddwolff <40377333+baddwolff@users.noreply.github.com>
2019-11-17 20:22:19 +0530

    Added ReccomSys

--- appServer.js
+++ appServer.js
@@ -14,6 +14,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
 
+
+function len(obj){
+    var len=0;
+    for(var i in obj){
+        len++
+    }
+    return len;
+}
+
+
+function euclidean_score(dataset,p1,p2){
+    var existp1p2 = {};
+    for(var key in dataset[p1]){
+        if(key in dataset[p2]){
+            existp1p2[key] = 1
+        }
+    }
+        if(len(existp1p2) ==0) return 0;
+        var sum=0;
+        for(var item in dataset[p1]){
+            if(item in dataset[p2]){
+                sum +=(Math.pow(dataset[p1][item]-dataset[p2][item],2));
+            }
+        }
+        
+        var sum_sqrt = 1/(1 +Math.sqrt(sum));    
+        return sum_sqrt;
+    
+}
+
 app.use(session({
     secret: 'SadTanookiNoises',
     resave: true,
@@ -23,7 +53,42 @@ app.use(session({
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
- 
+
+app.post('/api/v1/get_highscore',function(req,res){
+    var user=req.body.user;
+    var name=req.body.quiz;
+    var content = fs.readFileSync("data/user_data.json");
+    var dat = JSON.parse(content)
+    var flag=false;
+    var i=0;
+    for(; i < dat.users.length; i++)   
+    {
+        if(dat.users[i].username == user)
+        {
+        console.log("here");
+        flag=true;
+        break;
+    }
+    }
+    var thisuser;
+    console.log(dat.users[0]);
+    var arr=dat.users[i].quizes;
+    flag=false;
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i].hasOwnProperty(name)) {
+            thisuser=arr[i]
+            flag=true;
+            break;
+        }
+    }
+    if(flag){
+        return res.status(200).send(thisuser[name]);
+    }
+    else{
+        return res.status(200).send("0");
+    }
+});
+
 app.get('/api/v1/get_questions',function(req,res){
     var content = fs.readFileSync("data/quiz_data.json");
     var dat = JSON.parse(content);
@@ -125,8 +190,39 @@ app.post('/api/v1/rate_course',function(req,res){
     var rating=req.body.rate;
     var content = fs.readFileSync("data/ratings_data.json");
     var dat = JSON.parse(content);
-    dat.ratings.push({"course":course,"rating":rating});
+
+
+    var flag=false;
+    var i=0;
+    for(; i < dat.ratings.length; i++)   
+    {
+        if(dat.ratings[i].user == user)
+        {
+        flag=true;
+        break;
+    }
+    }
+    if(flag){
+        var payload={
+        }
+        payload[course]=rating;
+        console.log(dat.ratings[i].courses);
+        dat.ratings[i].courses[course]=rating;
+        fs.writeFile("data/ratings_data.json", JSON.stringify(dat));
+    }
+    else{
+        var payload={
+        }
+        payload[course]=rating
+        // arr.push(payload);
+        dat.ratings.push({"user":user,"courses":payload});
+        
+    }
+    
     fs.writeFile("data/ratings_data.json", JSON.stringify(dat));
+    
+    
+    //add course to user_data
     var content = fs.readFileSync("data/user_data.json");
     var dat = JSON.parse(content);
     var flag=false;
@@ -252,6 +348,94 @@ app.post('/api/v1/register',function(req, res){
     
 });
  
+app.post('/api/v1/recmon', function(req, response){
+var p1=req.body.user;
+var content = fs.readFileSync("data/ratings_data.json");
+var dat = JSON.parse(content);
+var db ={};
+for(var i=0; i < dat.ratings.length; i++)   
+    {
+        var item=dat.ratings[i];
+        db[item.user]={}
+        for(var lang in item.courses){
+            db[item.user][lang]=parseInt(item.courses[lang])
+        }
+    }
+var m1= -1;
+var m2= -1;
+var m3=-1;
+var f=p1;
+var s=p1;
+var t=p1;
+if(Object.keys(db[p1]).length==0){
+    var p1lang={}
+}
+else{
+    var p1lang=db[p1];
+}
+for(var p2 in db){
+    if(p2!=p1){
+        var x=euclidean_score(db,p1,p2)
+        if(x>m1){
+            t=s;
+            s=f;
+            f=p2;
+            m3=m2;
+            m2=m1;
+            m1=x;
+            
+        }
+        else{
+            if(x>m2){
+                t=s;
+                s=p2;
+                m3=m2;
+                m2=x;
+
+            }
+            else{
+                if(x>m3){
+                    t=p2;
+                    m3=x;
+                }
+            }
+        }
+    }
+}
+var arr=[];
+var res={};
+arr.push(f);
+arr.push(s);
+arr.push(t);
+for(var p2 in arr){
+    for(var lang in db[arr[p2]]){
+        if(!p1lang.hasOwnProperty(lang) & !res.hasOwnProperty()){
+            res[lang]=lang;
+            if(Object.keys(res).length>=3){
+                break;
+            }
+        }
+    }    
+    if(Object.keys(res).length>=3){
+        break;
+    }
+}
+if(Object.keys(res).length<3){
+    for(var p2 in arr){
+        for(var lang in db[arr[p2]]){   
+                res[lang]=lang;
+                if(Object.keys(res).length>=3){
+                    break;
+                }
+        }    
+        if(Object.keys(res).length>=3){
+            break;
+        }
+    }  
+}
+// console.log(res);
+return response.status(200).send(res);
+});
 
 
 var server = app.listen(8081, function(){
--- misc/test.js
+++ misc/test.js
@@ -1,3 +1,10 @@
+
+'use strict'
+var express = require("express");
+var bodyParser    = require('body-parser');
+var fs = require("fs");
+var session = require('express-session');
+var path = require('path');
 var dataset={
     'Lisa Rose': {
     'Lady in the Water': 2.5,
@@ -42,7 +49,7 @@ var dataset={
     'You, Me and Dupree':1.0,
     'Superman Returns':4.0}};
 
-var len  = function(obj){
+function len(obj){
     var len=0;
     for(var i in obj){
         len++
@@ -50,32 +57,28 @@ var len  = function(obj){
     return len;
 }
 
-//calculate the euclidean distance btw two item
-var euclidean_score  = function(dataset,p1,p2){
-    
-    var existp1p2 = {};//store item existing in both item//if dataset is in p1 and p2 
-    //store it in as one
+
+function euclidean_score(dataset,p1,p2){
+    var existp1p2 = {};
     for(var key in dataset[p1]){
         if(key in dataset[p2]){
             existp1p2[key] = 1
-        }if(len(existp1p2) ==0) return 0;//check if it has a datavar sum_of_euclidean_dist = [];//store the  euclidean distance
-        
-        //calculate the euclidean distance
-        for(item in dataset[p1]){
-            if(item in dataset[p2]){
-                sum_of_euclidean_dist.push(Math.pow(dataset[p1] [item]-dataset[p2][item],2));
-            }}
-        var sum=0;
-        for(var i=0;i<sum_of_euclidean_dist.length;i++){
-            sum +=sum_of_euclidean_dist[i]; //calculate the sum of the euclidean
-        }//since the sum will be small for familiar user
-        // and larger for non-familiar user 
-        //we make it exist btwn 0 and 1        var sum_sqrt = 1/(1 +Math.sqrt(sum));    return sum_sqrt;
+        }
     }
+        if(len(existp1p2) ==0) return 0;
+        var sum=0;
+        for(var item in dataset[p1]){
+            if(item in dataset[p2]){
+                sum +=(Math.pow(dataset[p1][item]-dataset[p2][item],2));
+            }
+        }
+        
+        var sum_sqrt = 1/(1 +Math.sqrt(sum));    
+        return sum_sqrt;
     
 }
 
-var pearson_correlation = function(dataset,p1,p2){
+function person_correlation(dataset,p1,p2){
         var existp1p2 = {};
         for(item in dataset[p1]){
             if(item in dataset[p2]){
@@ -83,25 +86,112 @@ var pearson_correlation = function(dataset,p1,p2){
             }
         }
         var num_existence = len(existp1p2);
-        if(num_existence ==0) return 0;//store the sum and the square sum of both p1 and p2
-        //store the product of both
+        if(num_existence ==0) return 0;
         var p1_sum=0,
             p2_sum=0,
             p1_sq_sum=0,
             p2_sq_sum=0,
             prod_p1p2 = 0;
-        //calculate the sum and square sum of each data point
-        //and also the product of both point
         for(var item in existp1p2){
             p1_sum += dataset[p1][item];
-            p2_sum += dataset[p2][item];p1_sq_sum += Math.pow(dataset[p1][item],2);
-            p2_sq_sum += Math.pow(dataset[p2][item],2);prod_p1p2 += dataset[p1][item]*dataset[p2][item];
+            p2_sum += dataset[p2][item];
+            p1_sq_sum += Math.pow(dataset[p1][item],2);
+            p2_sq_sum += Math.pow(dataset[p2][item],2);
+            prod_p1p2 += dataset[p1][item]*dataset[p2][item];
         }
-        var numerator =prod_p1p2 - (p1_sum*p2_sum/num_existence);var st1 = p1_sq_sum - Math.pow(p1_sum,2)/num_existence;
-        var st2 = p2_sq_sum -Math.pow(p2_sum,2)/num_existence;var denominator = Math.sqrt(st1*st2);if(denominator ==0) return 0;
+        var numerator =prod_p1p2 - (p1_sum*p2_sum/num_existence);
+        var st1 = p1_sq_sum - Math.pow(p1_sum,2)/num_existence;
+        var st2 = p2_sq_sum -Math.pow(p2_sum,2)/num_existence;
+        var denominator = Math.sqrt(st1*st2);
+        if(denominator ==0)return 0;
         else {
             var val = numerator / denominator;
             return val;
         }
         
 }
+
+
+
+
+var content = fs.readFileSync("data/ratings_data.json");
+var dat = JSON.parse(content);
+var db ={};
+for(var i=0; i < dat.ratings.length; i++)   
+    {
+        var item=dat.ratings[i];
+        db[item.user]={}
+        for(var lang in item.courses){
+            db[item.user][lang]=parseInt(item.courses[lang])
+        }
+    }
+var p1="aditya";
+var m1= -1;
+var m2= -1;
+var m3=-1;
+var f=p1;
+var s=p1;
+var t=p1;
+var p1lang=db[p1];
+for(var p2 in db){
+    if(p2!=p1){
+        var x=euclidean_score(db,p1,p2)
+        if(x>m1){
+            t=s;
+            s=f;
+            f=p2;
+            m3=m2;
+            m2=m1;
+            m1=x;
+            
+        }
+        else{
+            if(x>m2){
+                t=s;
+                s=p2;
+                m3=m2;
+                m2=x;
+
+            }
+            else{
+                if(x>m3){
+                    t=p2;
+                    m3=x;
+                }
+            }
+        }
+    }
+    // console.log(p2,f,s,t);
+}
+var arr=[];
+var res={};
+arr.push(f);
+arr.push(s);
+arr.push(t);
+for(var p2 in arr){
+    for(var lang in db[arr[p2]]){
+        if(!p1lang.hasOwnProperty(lang) & !res.hasOwnProperty()){
+            res[lang]=lang;
+            if(Object.keys(res).length>=3){
+                break;
+            }
+        }
+    }    
+    if(Object.keys(res).length>=3){
+        break;
+    }
+}
+if(Object.keys(res).length<3){
+    for(var p2 in arr){
+        for(var lang in db[arr[p2]]){   
+                res[lang]=lang;
+                if(Object.keys(res).length>=3){
+                    break;
+                }
+        }    
+        if(Object.keys(res).length>=3){
+            break;
+        }
+    }  
+}
+console.log(res);
